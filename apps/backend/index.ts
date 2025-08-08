b93e9c3dec1ad67d1cda0194e09c9956ca7f9b1b
Your Name <rudrakshg1914@gmail.com>
2025-08-08 12:47:20 +0530

    make backend running on port 8080

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import {prisma} from "db/client"; 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 
 
 app.post("/user",async(req,res)=>{
